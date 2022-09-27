623f6b51e996fd5342f62ea2f36ba89062ba1099
Vari <varianto.prawira@gopay.co.id>
2022-09-27 16:57:25 +0700

    test: get multiple users from the list

--- src/test/java/com/mycompany/user/UserServiceImpTest.java
+++ src/test/java/com/mycompany/user/UserServiceImpTest.java
@@ -1,5 +1,6 @@
 package com.mycompany.user;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 import java.util.ArrayList;
@@ -17,5 +18,21 @@ public class UserServiceImpTest {
         ArrayList<User> users = userService.getUsers();
 
         assertFalse(users.isEmpty());
+    }
+
+    @Test
+    public void givenEmptyUserListWhenThreeUsersAreAddedThenThereShouldBeThreeUsersInTheList() {
+        UserServiceImp userService = new UserServiceImp();
+        User user1 = new User("1", "vari", "angga", "vari.angga@example.com");
+        User user2 = new User("2", "budi", "", "budi@example.com");
+        User user3 = new User("3", "joni", "andi", "joniandi@example.com");
+
+        userService.addUser(user1);
+        userService.addUser(user2);
+        userService.addUser(user3);
+
+        ArrayList<User> users = userService.getUsers();
+
+        assertEquals(3, users.size());
     }
 }
